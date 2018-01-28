e92f4c306d4939dcf4e7a386225d5c26b18a3b85
StephenOTT <stephenrussett@gmail.com>
2018-01-28 16:07:36 -0500

    test

--- docs/pages/myscript.js
+++ docs/pages/myscript.js
@@ -1,35 +1,35 @@
 
 var elements = document.getElementsByClassName("bpmn");
 console.log(elements)
-var viewers = [];
-var xhrs = [];
+// var viewers = [];
+// var xhrs = [];
 
-  for (var i in elements) {
+//   for (var i in elements) {
 
-    viewers[i] = new BpmnJS({ container: '#'+i.id });
-    // var viewer = new BpmnJS({ container: '#canvas' },{});
+//     viewers[i] = new BpmnJS({ container: '#'+i.id });
+//     // var viewer = new BpmnJS({ container: '#canvas' },{});
 
-    xhrs[i] = new XMLHttpRequest();
+//     xhrs[i] = new XMLHttpRequest();
 
-    xhrs[i].onreadystatechange = function() {
-      if (xhrs[i].readyState === 4) {
-        viewers[i].importXML(xhrs[i].response, function(err) {
-          if (err) {
-                // import failed :-(
-              } else {
-                // we did well!
-                var canvas = viewers[i].get(i.id);
-                canvas.zoom('fit-viewport');
-              }
-        });
-      }
-    };
+//     xhrs[i].onreadystatechange = function() {
+//       if (xhrs[i].readyState === 4) {
+//         viewers[i].importXML(xhrs[i].response, function(err) {
+//           if (err) {
+//                 // import failed :-(
+//               } else {
+//                 // we did well!
+//                 var canvas = viewers[i].get(i.id);
+//                 canvas.zoom('fit-viewport');
+//               }
+//         });
+//       }
+//     };
 
-    xhrs[i].open('GET', i.dataset.bpmn, true);
-    xhrs[i].send(null);
+//     xhrs[i].open('GET', i.dataset.bpmn, true);
+//     xhrs[i].send(null);
 
 
-}
+// }
 
 
 
